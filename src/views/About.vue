6f4c216845de12b015efe93f6a36ae53854758c9
HENG PAUL <rahveiz@gmail.com>
2020-12-06 00:47:44 +0100

    ouio

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,82 +1,10 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <router-link to="Login">Login</router-link>
-    <v-btn @click="triggerPushNotification"> Sub to notifications</v-btn>
-    <v-btn @click="testPush">Test Push</v-btn>
-    <p>Sub status: {{ status }}</p>
   </div>
 </template>
 <script>
-import { mapGetters } from "vuex";
-import { urlBase64ToUint8Array } from "@/helpers";
-import { users } from "@/firebase";
-
 export default {
-  name: "About",
-  async mounted() {
-    console.log("plop");
-    this.status = await this.subStatus();
-  },
-  data: () => ({
-    status: "..."
-  }),
-  methods: {
-    async triggerPushNotification() {
-      const publicVapidKey = process.env.VUE_APP_VAPID_KEY;
-      if ("serviceWorker" in navigator) {
-        const register = await navigator.serviceWorker.ready;
-
-        const subscription = await register.pushManager.subscribe({
-          userVisibleOnly: true,
-          applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
-        });
-
-        await fetch(`${process.env.VUE_APP_PUSH_ENDPOINT}/subscribe`, {
-          method: "POST",
-          body: JSON.stringify(subscription),
-          headers: {
-            "Content-Type": "application/json"
-          }
-        });
-
-        if (this.uid) {
-          await users
-            .doc(this.uid)
-            .update({ push: JSON.stringify(subscription) });
-        }
-      } else {
-        console.error("Service workers are not supported in this browser");
-      }
-    },
-    async testPush() {
-      await fetch(`${process.env.VUE_APP_PUSH_ENDPOINT}/push`, {
-        method: "POST",
-        body: JSON.stringify({ uid: this.uid }),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      });
-    },
-    async subStatus() {
-      if (!("Notification" in window && navigator.serviceWorker)) {
-        return "Not available";
-      }
-      let permission = await Notification.requestPermission();
-      if (permission != "granted") {
-        return "Not granted";
-      }
-      let sub = await navigator.serviceWorker.ready.then(reg => {
-        return reg.pushManager.getSubscription();
-      });
-      if (sub) {
-        return "Subbed";
-      }
-      return "Not subbed";
-    }
-  },
-  computed: {
-    ...mapGetters(["uid"])
-  }
+  name: "About"
 };
 </script>
