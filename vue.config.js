3fd2642f3303cc5c205cd3bb2d290872eb243c55
HENG PAUL <rahveiz@gmail.com>
2020-12-08 21:35:55 +0100

    add icons

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,45 @@ module.exports = {
     workboxPluginMode: "InjectManifest",
     workboxOptions: {
       swSrc: "service-worker.js"
+    },
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "white",
+    themeColor: "#FFFFFF",
+    manifestOptions: {
+      name: "Pocket-Kulture",
+      shotName: "Pocket-Kulture",
+      icons: [
+        {
+          src: "./img/icons/PK_120.png",
+          sizes: "120x120",
+          type: "image/png"
+        },
+        {
+          src: "./img/icons/PK_128.png",
+          sizes: "128x128",
+          type: "image/png"
+        },
+        {
+          src: "./img/icons/PK_180.png",
+          sizes: "180x180",
+          type: "image/png"
+        },
+        {
+          src: "./img/icons/PK_192.png",
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          src: "./img/icons/PK_512.png",
+          sizes: "512x512",
+          type: "image/png"
+        }
+      ]
+    },
+    iconPaths: {
+      appleTouchIcon: "img/icons/PK_180.png",
+      favicon16: "./img/icons/PK_16.png",
+      favicon32: "./img/icons/PK_32.png"
     }
   }
 };
