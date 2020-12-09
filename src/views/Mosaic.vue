638c412211a78e96c8e73b5b0cda82a85192d230
Timothée Oliger <tim@bachaner.fr>
2020-12-09 16:57:50 +0100

    clean

--- src/views/Mosaic.vue
+++ src/views/Mosaic.vue
@@ -48,7 +48,6 @@ export default {
     }
   },
   mounted() {
-    console.log(this.userInterests);
     if (this.userInterests.length > 0)
       topics
         .where("tags", "array-contains-any", this.userInterests)
