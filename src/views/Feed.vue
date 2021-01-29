8540ed1a9c08b591344e59e9fecf765edc9f214f
Paul Heng <rahveiz@gmail.com>
2021-01-29 15:51:04 +0000

    prettier c'est pas pour les chiens

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -15,18 +15,31 @@ n<template>
           ></v-autocomplete>
         </v-col>
         <v-col v-for="(item, i) in top" :key="i" cols="12">
-          <v-card v-if="check_filter(item.id)" @click="topic(item.id)" :color="item.color" dark>
+          <v-card
+            v-if="check_filter(item.id)"
+            @click="topic(item.id)"
+            :color="item.color"
+            dark
+          >
             <div class="d-flex flex-no-wrap justify-space-between">
               <div>
                 <v-card-title class="text-h6">{{ item.title }}</v-card-title>
 
                 <v-card-subtitle v-text="item.artist"></v-card-subtitle>
                 <v-card-actions>
-                  <v-btn icon @click.stop="upvote(item.id)" v-bind:class="{ no_clickable: !isUserAuth }">
+                  <v-btn
+                    icon
+                    @click.stop="upvote(item.id)"
+                    v-bind:class="{ no_clickable: !isUserAuth }"
+                  >
                     <v-icon>mdi-arrow-up-bold-outline</v-icon>
                   </v-btn>
                   <span>{{ item.vote.up.length }}</span>
-                  <v-btn icon @click.stop="downvote(item.id)" v-bind:class="{ no_clickable: !isUserAuth }">
+                  <v-btn
+                    icon
+                    @click.stop="downvote(item.id)"
+                    v-bind:class="{ no_clickable: !isUserAuth }"
+                  >
                     <v-icon>mdi-arrow-down-bold-outline</v-icon>
                   </v-btn>
                   <span>{{ item.vote.down.length }}</span>
@@ -69,41 +82,53 @@ export default {
     },
     upvote(id) {
       if (this.isUserAuth) {
-      topics.doc(id).get().then(data => {
-        data = data.data();
-        if (data.vote.up.indexOf(this.user.uid) === -1) {
-          let pos = data.vote.down.indexOf(this.user.uid);
-          if (pos !== -1) {
-            data.vote.down.splice(pos, 1);
-          }
-          data.vote.up.push(this.user.uid);
-          topics.doc(id).update({ vote: data.vote }).then(() => this.top_up()); 
-        }
-      });
+        topics
+          .doc(id)
+          .get()
+          .then(data => {
+            data = data.data();
+            if (data.vote.up.indexOf(this.user.uid) === -1) {
+              let pos = data.vote.down.indexOf(this.user.uid);
+              if (pos !== -1) {
+                data.vote.down.splice(pos, 1);
+              }
+              data.vote.up.push(this.user.uid);
+              topics
+                .doc(id)
+                .update({ vote: data.vote })
+                .then(() => this.top_up());
+            }
+          });
       }
     },
     downvote(id) {
       if (this.isUserAuth) {
-      topics.doc(id).get().then(data => {
-        data = data.data();
-        if (data.vote.down.indexOf(this.user.uid) === -1) {
-          let pos = data.vote.up.indexOf(this.user.uid);
-          if (pos !== -1) {
-            data.vote.up.splice(pos, 1);
-          }
-          data.vote.down.push(this.user.uid);
-          topics.doc(id).update({ vote: data.vote }).then(() => this.top_up()); 
-        }
-      });
+        topics
+          .doc(id)
+          .get()
+          .then(data => {
+            data = data.data();
+            if (data.vote.down.indexOf(this.user.uid) === -1) {
+              let pos = data.vote.up.indexOf(this.user.uid);
+              if (pos !== -1) {
+                data.vote.up.splice(pos, 1);
+              }
+              data.vote.down.push(this.user.uid);
+              topics
+                .doc(id)
+                .update({ vote: data.vote })
+                .then(() => this.top_up());
+            }
+          });
       }
     },
     top_up() {
       topics.get().then(data => {
-	      this.top = data.docs.map(doc => {
-		let res = doc.data();
-		res.id = doc.id;
-		return res;
-	      });
+        this.top = data.docs.map(doc => {
+          let res = doc.data();
+          res.id = doc.id;
+          return res;
+        });
         this.top.forEach(top => {
           top.tags.forEach(tag => {
             if (this.tag_f.indexOf(tag) === -1) {
@@ -128,18 +153,27 @@ export default {
         if (ltop.id == top_id) {
           top_ind = i;
           return;
-        } 
+        }
       });
       let res = false;
       this.values.forEach(tag => {
-        if (this.top[top_ind].title === tag) { res = true; return; }
-        if (this.top[top_ind].artist === tag) { res = true; return; }
+        if (this.top[top_ind].title === tag) {
+          res = true;
+          return;
+        }
+        if (this.top[top_ind].artist === tag) {
+          res = true;
+          return;
+        }
         this.top[top_ind].tags.forEach(t => {
-          if (t === tag) { res = true; return; }
+          if (t === tag) {
+            res = true;
+            return;
+          }
         });
       });
       return res;
-    } 
+    }
   },
   mounted() {
     this.top_up();
@@ -154,7 +188,7 @@ export default {
   computed: {
     ...mapGetters(["user", "isUserAuth"]),
     filters() {
-      return [ ...this.tag_f, ...this.artist_f, ...this.title_f ];
+      return [...this.tag_f, ...this.artist_f, ...this.title_f];
     }
   }
 };
